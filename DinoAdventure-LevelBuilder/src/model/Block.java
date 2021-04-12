80280f9d504abfed36663172856cab99af67a9e1
Christopher Zuehlke <czueh332@students.bju.edu>
2021-04-12 19:17:01 -0400

    updated level builder classes

--- DinoAdventure-LevelBuilder/src/model/Block.java
+++ DinoAdventure-LevelBuilder/src/model/Block.java
@@ -2,4 +2,14 @@ package model;
 
 public class Block extends Box {
 
+    //initiat default texture
+    private String texture = "assets/images/world/ground-2.png";
+
+    public String getTexture() {
+        return texture;
+    }
+
+    public void setTexture(String texture) {
+        this.texture = texture;
+    }
 }
--- DinoAdventure-LevelBuilder/src/model/LevelTest.java
+++ DinoAdventure-LevelBuilder/src/model/LevelTest.java
@@ -12,7 +12,7 @@ public class LevelTest {
     public void test_level_findEntity() {
         Level level = new Level();
 
-        Enemy enemy = new WanderingEnemy();
+        Enemy enemy = new Enemy();
 
         enemy.setId(3);
 
@@ -37,7 +37,7 @@ public class LevelTest {
     public void test_level_methods() throws Exception {
         Level level = new Level();
 
-        Enemy enemy = new WanderingEnemy();
+        Enemy enemy = new Enemy();
         enemy.setId(2);
         enemy.centerPoint().setXY(10, 25);
 
@@ -65,7 +65,7 @@ public class LevelTest {
     public void test_Level_Save_Load() throws Exception {
         Level level = new Level();
 
-        Enemy enemy = new WanderingEnemy();
+        Enemy enemy = new Enemy();
         enemy.setId(2);
         enemy.centerPoint().setXY(10, 25);
 
@@ -93,7 +93,7 @@ public class LevelTest {
         level2.load("Custom1.dat");
 
         Entity enemy2 = level2.findEntity(2);
-        Box block2 = level2.findBlock(1);
+        Block block2 = level2.findBlock(1);
 
         //test the loaded level
         // assertEquals("Custom1", level2.getLevelName());
