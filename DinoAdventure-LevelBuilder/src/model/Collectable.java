3c629ba576821948afb4967e515c733deca54cc7
Christopher Zuehlke <czueh332@students.bju.edu>
2021-04-21 22:29:40 -0400

    Fixed the random flags in level2 and fixed bugs

--- DinoAdventure-LevelBuilder/src/model/Collectable.java
+++ DinoAdventure-LevelBuilder/src/model/Collectable.java
@@ -23,79 +23,25 @@ public class Collectable extends Entity {
     public CollectableType getType() {
         return type;
     }
+
     /**
      * gets the type of the colletable and returns a string value of it's type
-     * @return String value of enemy type 
+     * 
+     * @return String value of collectable type
      */
     public String getStringType() {
-     
 
         if (type == CollectableType.Coin) {
             return "Coin";
-        }
-        else if (type == CollectableType.FeatherPowerup) {
-            return "featherpowerup";
-        }
-        else if (type == CollectableType.SpeedPowerup) {
-            return "speedpowerup";
-        }
-        else if (type == CollectableType.CoinPowerup) {
-            return "coinpowerup";
-        }
-        else if (type == CollectableType.HealthPowerup) {
-            return "healthpowerup";
+        } else if (type == CollectableType.FeatherPowerup) {
+            return "FeatherPowerup";
+        } else if (type == CollectableType.SpeedPowerup) {
+            return "SpeedPowerup";
+        } else if (type == CollectableType.CoinPowerup) {
+            return "CoinPowerup";
+        } else if (type == CollectableType.HealthPowerup) {
+            return "HealthPowerup";
         }
         return "";
     }
-    }
-
-    // public void collect() {
-    //     switch(type) {
-    //         case Coin:
-    //             Game.instance().getPlayer().scoreProperty().set(Game.instance().getPlayer().scoreProperty().get() + 25);
-    //             break;
-
-    //         case SpeedPowerup:
-    //             int speedChange = 3;
-    //             Game.instance().getPlayer().setMaxSpeed(Game.instance().getPlayer().getMaxSpeed() + speedChange);
-    //             new Timeline(new KeyFrame(Duration.seconds(10), e -> {
-    //                 Game.instance().getPlayer().setMaxSpeed(Game.instance().getPlayer().getMaxSpeed() - speedChange);
-    //             })).play();
-    //             break;
-
-    //         case FeatherPowerup:
-    //             int jumpChange = 4;
-    //             Game.instance().getPlayer().setMaxJumpHeight(Game.instance().getPlayer().getMaxJumpHeight() + jumpChange);
-    //             new Timeline(new KeyFrame(Duration.seconds(10), e -> {
-    //                 Game.instance().getPlayer().setMaxJumpHeight(Game.instance().getPlayer().getMaxJumpHeight() - jumpChange);
-    //             })).play();
-    //             break;
-
-    //         case HealthPowerup:
-    //             int healthChange = 5;
-    //             Game.instance().getPlayer().setHealth(Game.instance().getPlayer().getHealth() + healthChange);
-    //             break;
-
-    //         case CoinPowerup:
-    //             int scoreChange = 100;
-    //             Game.instance().getPlayer().setScore(Game.instance().getPlayer().getScore() + scoreChange);
-    //             break;
-
-    //         default:
-    //             break;
-    //     }
-    //     Platform.runLater(() -> Game.instance().getCurrentLevel().getCollectables().remove(this));
-    // }
-
-    // public void tick() {
-
-    //     // Apply generic Collectable physics updates
-    //     super.tick();
-
-    //     if(Game.instance().getPlayer().overlaps(this)) {
-    //         collect();
-    //     }
-
-    // }
-
-// }
+}
